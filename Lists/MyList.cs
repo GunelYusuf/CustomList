f6740a9c25065de17d898f0f352a5523cf774da4
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2021-10-26 23:46:11 +0400

    Custom Of Methods

---
+++ Lists/MyList.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    public class MyList<T>
+    {
+        private T[] arr;
+        public int count { get; set; }
+
+        public MyList()
+        {
+            arr = new T[10];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                try
+                {
+                    if (index < count)
+                    {
+                        return arr[index];
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Bu index movcud deyil");
+                    }
+                }
+                catch (Exception)
+                {
+                    throw new ArgumentOutOfRangeException("Bu index movcud deyil");
+                }
+
+            }
+            set
+            {
+                try
+                {
+                   arr[index] = value;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Out of Range");
+                }
+            }
+        }
+
+        
+
+        public void Add(T obj)
+        {
+            if ((count!=0)&&(count%10==0))
+            {
+                ArrayResize();
+            }
+            arr[count] = obj;
+            count++;
+        }
+
+        private void ArrayResize()
+        {
+            Array.Resize(ref arr, arr.Length + 10);
+        }
+
+        public void Clear()
+        {
+            arr = new T[10];
+            count = 0;
+        }
+
+        public void AddRange(IEnumerable<T> list)
+        {
+            foreach (var item in list)
+            {
+                Add(item);
+            }
+        }
+
+        public T Find(Predicate<T> func)
+        {
+            foreach (var item in arr)
+            {
+                if (func(item))
+                {
+                    return item;
+                }
+            }return default;
+            
+        }
+
+        public bool Contains(T obj)
+        {
+            foreach (var item in arr)
+            {
+                if (Comparer<T>.Default.Compare(item,obj)==0)
+                {
+                    return true;
+                }
+            }return false;
+            
+        }
+
+        public int Count
+        {
+            get
+            {
+                foreach (var item in arr)
+                {
+                   if (item!=null)
+                   {
+                        Console.WriteLine(item);
+                   }
+                }return count;
+            }
+        }
+
+
+        public bool Remove(T obj)
+        {
+            foreach (var item in arr)
+            {
+                if (Contains(obj))
+                {
+
+
+                }
+
+            }
+            return false;
+        }
+
+
+        public int IndexOf(T obj)
+        {
+            foreach (var item in arr)
+            {
+
+            }
+
+
+            return default;
+        }
+
+
+    }
+}
---
+++ Lists/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> list = new MyList<string>();
+            list.Add("Gunell1");
+            list.Add("Sabinaa2");
+            list.Add("Emill3");
+            list.Add("Elcinn4");
+            list.Add("Rauff5");
+            list.Add("Aysenn6");
+            List<int> aa = new List<int>();
+       
+            //aa.Add(2);
+            //aa.Add(3);
+            //aa.Add(4);
+          
+            //Console.WriteLine(list[0]);
+            //Console.WriteLine(list[1]);
+            //Console.WriteLine(list[2]);
+            //Console.WriteLine(list[3]);
+            //Console.WriteLine(list[4]);
+            //Console.WriteLine(list[5]);
+            
+            //Console.WriteLine(list.Remove("Sabinaa2"));
+            Console.WriteLine(list.Count);
+        }
+    }
+}
